6665975a453bc02c190ac08a01e21fe221de927c
Prasad <prasada@ttechlab.com>
2023-06-13 16:11:41 +0530

    cors client

---
+++ src/main/java/com/ttechlab/cors/SpringbootCorsClientApplication.java
@@ -0,0 +1,20 @@
+package com.ttechlab.cors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+@Controller
+public class SpringbootCorsClientApplication {
+
+	@GetMapping("/")
+	public String home() {
+		return "home";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCorsClientApplication.class, args);
+	}
+}
